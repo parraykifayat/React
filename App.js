9043d70201e1a0d0a08a053a64a4f18c6db81d11
parraykifayat <38093085+parraykifayat@users.noreply.github.com>
2024-08-27 09:45:52 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,24 @@
+const parent = React.createElement("div",{id:"parent"},
+    [
+        React.createElement("div",{id:"child"},
+            //if i have to create a siblings what i will do is i create array
+            
+            [
+                React.createElement("h1",{},"this is heading 1 "),
+                React.createElement("h2",{},"this is heading 2"),
+            ]
+        ),
+        React.createElement("div",{id:"child"},
+            //if i have to create a siblings what i will do is i create array
+            [
+                React.createElement("h3",{},"this is heading 2 "),
+                React.createElement("h4",{},"this is heading 2"),
+            ]
+        )
+    ]
+    
+);
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
