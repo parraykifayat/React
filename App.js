660c94c4a862b54571fd1bf83020d3a80fab4193
parraykifayat <38093085+parraykifayat@users.noreply.github.com>
2024-08-29 10:04:56 +0530

    installing the parcel and  configure the start i  the scripts

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div",{id:"parent"},
     [
         React.createElement("div",{id:"child"},
             //if i have to create a siblings what i will do is i create array
             
             [
-                React.createElement("h1",{},"this is heading 1 "),
+                React.createElement("h1",{},"this is react course "),
                 React.createElement("h2",{},"this is heading 2"),
             ]
         ),
