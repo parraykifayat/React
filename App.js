23288ee413d1b1b18d344d6c85679f7380aef700
parraykifayat <38093085+parraykifayat@users.noreply.github.com>
2024-08-30 12:15:28 +0530

    creating componets and arrow functions

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
-    [
-        React.createElement("div",{id:"child"},
-            //if i have to create a siblings what i will do is i create array
-            
-            [
-                React.createElement("h1",{},"this is react course "),
-                React.createElement("h2",{},"this is heading 2"),
-            ]
-        ),
-        React.createElement("div",{id:"child"},
-            //if i have to create a siblings what i will do is i create array
-            [
-                React.createElement("h3",{},"this is heading 2 "),
-                React.createElement("h4",{},"this is heading 2"),
-            ]
-        )
-    ]
-    
+const Title=()=>(
+    <h1 className="head" tabIndex="5">
+        Namaste React using jsx
+    </h1>
 );
-console.log(parent);
+const HeadingComponent = () =>(
+    <div id="container">
+        <Title/>
+        <h1 className="heading">
+            Hello this is component
+        </h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>);
